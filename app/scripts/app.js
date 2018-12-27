e781b3237c3870466583baaad29c39fb8da3e3cb
Jungpil YU <31795854+jungpilyu@users.noreply.github.com>
2018-12-27 23:02:34 +0900

    [Quiz3] first-thumb-start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,10 @@
 /*
 Instructions:
-(1) Rewrite get with the Fetch API: https://davidwalsh.name/fetch
-(2) Finish the getJSON method. getJSON should take a URL and return the parsed JSON response.
-  (a) getJSON needs to return a Promise!
-(3) Test by console.logging the response and by passing the query string from getJSON to addSearchHeader.
-(4) Handle errors by passing "unknown" to addSearchHeader.
+(1) Get the planet data and add the search header.
+(2) Create the first thumbnail with createPlanetThumb(data)
+(3) Handle errors!
+  (a) Pass 'unknown' to the search header.
+  (b) console.log the error.
  */
 
 // Inline configuration for jshint below. Prevents `gulp jshint` from failing with quiz starter code.
@@ -24,17 +24,26 @@ Instructions:
   }
 
   /**
-   * XHR wrapped in a Promise using Fetch.
+   * Helper function to create a planet thumbnail.
+   * @param  {Object} data - The raw data describing the planet.
+   */
+  function createPlanetThumb(data) {
+    var pT = document.createElement('planet-thumb');
+    for (var d in data) {
+      pT[d] = data[d];
+    }
+    home.appendChild(pT);
+  }
+
+  /**
+   * XHR wrapped in a promise
    * @param  {String} url - The URL to fetch.
    * @return {Promise}    - A Promise that resolves when the XHR succeeds and fails otherwise.
    */
   function get(url) {
-    /*
-    Use the Fetch API to GET a URL.
-    Return the fetch.
-    Your code goes here!
-     */
-    return fetch(url, {method: 'get'});
+    return fetch(url, {
+      method: 'get'
+    });
   }
 
   /**
@@ -43,33 +52,17 @@ Instructions:
    * @return {Promise}    - A promise that passes the parsed JSON response.
    */
   function getJSON(url) {
-    /*
-    Return a Promise that gets a URL and parses the JSON response. Use your get method!
-    Your code goes here!
-     */
-     return get(url).then(function(response) {
-      // Handle network errors
-      if (!response.ok) {
-        throw Error(response.statusText ? response.statusText : 'Unknown network error')
-      }
-      
-       return response.json();
-     });
+    return get(url).then(function(response) {
+      return response.json();
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
     /*
-    Uncomment the next line when you're ready to test!
-    Don't forget to chain with a .then and a .catch!
-    Your code goes here too!
+    Uncomment the next line and start here when you're ready to add the first thumbnail!
+    Your code goes here!
      */
-    getJSON('../data/earth-like-results.json').then(function(response) {
-      console.log(response);
-      addSearchHeader(response.query);
-    }).catch(function(error) {
-      console.log(error);
-      addSearchHeader('unknown');
-    });
+    // getJSON('../data/earth-like-results.json')
   });
 })(document);

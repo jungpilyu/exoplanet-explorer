ee5cb41d288d7ee6db8a313bf26bcc044375ecf4
Jungpil YU <31795854+jungpilyu@users.noreply.github.com>
2018-12-30 22:08:57 +0900

    [Quiz5] Complete map-start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,14 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json').then(function(response) {
+      response.results.map(function(planet) {
+        getJSON(planet).then(function(p) {
+          createPlanetThumb(p);
+        });
+      });
+    }).catch( function(e) {
+      console.log(e);
+    });
   });
 })(document);
